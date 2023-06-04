53aa1b341ee17d8ab6c61d1a9418d5b9912745f0
leonseet <kiangyeow@hotmail.com>
2023-06-04 11:16:22 +0800

    updated typescript error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css"
 import Providers from "./Providers"
 import { Toaster } from "@/components/ui/toaster"
-import Preloader from "@/components/Preloader"
+// import Preloader from "@/components/Preloader"
 
 export const metadata = {
   title: "Kanban",
