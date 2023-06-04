72beec413c85e8b3c2f45745e47b406b5b5f5591
leonseet <kiangyeow@hotmail.com>
2023-06-04 11:11:33 +0800

    updated typescript error

--- app/api/column-create/route.ts
+++ app/api/column-create/route.ts
@@ -3,12 +3,11 @@ import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
   try {
-    const { title, color, boardId, rank } = await req.json()
+    const { title, boardId, rank } = await req.json()
 
     const data = await db.column.create({
       data: {
         title,
-        color,
         boardId,
         rank,
       },
@@ -19,7 +18,6 @@ export async function POST(req: Request) {
       data: {
         id: data.id,
         title: data.title,
-        color: data.color,
         boardId: data.boardId,
         rank: data.rank,
       },
--- app/api/subtask-upsert/route.ts
+++ app/api/subtask-upsert/route.ts
@@ -3,13 +3,15 @@ import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
   try {
-    const { description, checked, subtaskId, taskId } = await req.json()
+    const { description, checked, subtaskId, taskId, rank, task } = await req.json()
 
     const data = await db.subtask.upsert({
       create: {
         taskId,
         checked,
         description,
+        rank,
+        task,
       },
       where: {
         id: subtaskId,
